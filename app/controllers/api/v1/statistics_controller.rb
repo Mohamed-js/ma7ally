1ffe74a2067b3b614eed7bc20609ccf8c1231ea7
Mohammed <atefhamosa@gmail.com>
2021-08-14 01:02:11 +0200

    Render statistics as JSON

--- app/controllers/api/v1/statistics_controller.rb
+++ app/controllers/api/v1/statistics_controller.rb
@@ -5,62 +5,80 @@ class Api::V1::StatisticsController < ApplicationController
     def index
         # SOME NEEDED VARS TO HELP LATER
         trader_orders = @trader.orders
+        trader_order_items = @trader.order_items
         trader_cart_items = Cart.where(trader_id: @trader.id)
         this_month = Date.today.beginning_of_month
 
-        # Orders' total price count
-        total_revenue = trader_orders.sum("total") # The result ------ Success with good results
+        # # ----------------------------------------------------------------------------
+        # # ----------------------------------------------------------------------------
 
-        # # Orders' total price count this month
-        this_month_revenue = trader_orders.where('created_at > ?', (this_month)).sum("total") # The result ------ Success with good results
+        total_profits = trader_orders.sum("total")
+        month_profits = trader_orders.where('created_at > ?', (this_month)).sum("total")
 
         # # ----------------------------------------------------------------------------
         # # ----------------------------------------------------------------------------
 
-        # # Orders count
-        total_num_orders = trader_orders.count # The result ------ Success with good results
-        total_num_order_items = @trader.order_items.count # The result ------ Success with good results
+        total_orders = trader_orders.count
+        total_orders_items = trader_order_items.count
 
-        # # Orders count this month
-        this_month_num_orders = trader_orders.where('created_at > ?', (this_month)).count # The result  ------ Success with good results
-        this_month_num_order_items = @trader.order_items.where('created_at > ?', (this_month)).count # The result  ------ Success with good results
+        month_orders = trader_orders.where('created_at > ?', (this_month)).count
+        month_orders_items = trader_order_items.where('created_at > ?', (this_month)).count
 
         # # ----------------------------------------------------------------------------
         # # ----------------------------------------------------------------------------
 
         # # Unique orders count in user_id
-        customer_total_active_number = trader_orders.count('DISTINCT user_id') # The result ------ Success with good results
-
+        active_users = trader_orders.count('DISTINCT user_id') # The result
 
         # # (Unique carts  - active members) .count = non active carts
-        active_members_ids = trader_orders.group(:user_id).count.keys.map{|key| key.to_i}
-        inactive_member_ids = trader_cart_items.group(:user_id).count.keys  - active_members_ids
-        customer_total_inactive_number = inactive_member_ids.length # The result ------ Success with good results
+        active_users_ids = Order.where(trader_id: @trader.id).group(:user_id).sum('total').keys.map{|key| key.to_i}
+        inactive_users_ids = trader_cart_items.group(:user_id).count.keys
+        inactive_users = inactive_users_ids.length # The result 
 
         # # ----------------------------------------------------------------------------
         # # ----------------------------------------------------------------------------
 
-        # #### TOP ITEMS UNITS SOLD IN ORDERS  ### EX)): ordered in 20 order 2 each order => higher units === COUNTS THE ITEMS ORDERED times ever
-        top_products_in_orders_total_units = OrderItem.where(trader_id: @trader.id).group(:item_id).order('sum_quantity desc').limit(10).sum('quantity')  # The result ------ Success with good results
+        store_users_count = active_users + inactive_users
+        store_items_count = @trader.items.count
 
-        # Sort them according to the count in carts ## JUST OCCERENCE REGARDLESS OF ITS TOTAL QUANTITY ==> NEEDED BUT NOT SOLD
-        top_products_in_carts_with_count = Cart.where(trader_id: @trader.id).group(:item_id).order('count_all desc').limit(10).count  # The result ------ Success with good results
+        # # ----------------------------------------------------------------------------
+        # # ----------------------------------------------------------------------------
 
-        # Orders' total revenue for a specific category
-        category_total_revenue = @trader.order_items.left_joins(:item).where("items.category_id = #{params[:category_id]}").sum('total')
+        most_popular_products = trader_order_items.joins(:item).select(:total, :name).group(:name).order('sum_quantity desc').limit(10).sum(:quantity)
+        most_profitable_products = trader_order_items.joins(:item).select(:total, :name).group(:name).order('sum_total desc').limit(10).sum(:total)
 
-        # Orders' total revenue for all categories as pairs [category_name: total_revenue]
-        category_revenue_pairs = @trader.order_items.joins('INNER JOIN items ON order_items.item_id = items.id LEFT JOIN categories ON items.category_id = categories.id').select('order_items.total, categories.name').group('categories.name').sum('total').to_a
+        category_revenue_pairs = trader_order_items.joins('INNER JOIN items ON order_items.item_id = items.id LEFT JOIN categories ON items.category_id = categories.id').select('order_items.total, categories.name').group('categories.name').sum('total').to_a # The result
 
-
-        # Get traders order items and group by item_id the count of the value
-        top_products_in_orders_value = @trader.order_items.joins(:item).select('total, name').group('name').order('sum_total desc').limit(10).sum('total')
-
-        # Top ordered products in a category 
-        top_products_in_orders_total_units_for_category = @trader.order_items.joins(:item).where("items.category_id = #{params[:category_id]}").select('name, quantity').group(:name).order('sum_quantity desc').limit(10).sum('quantity')
-
-        render
+        render json: { 
+            store: {
+                active_users: active_users,
+                inactive_users: inactive_users,
+                users: store_users_count,
+                items: store_items_count,
+                total_profits: total_profits,
+                month_profits: month_profits,
+                total_orders: total_orders,
+                total_orders_items: total_orders_items,
+                month_orders: month_orders,
+                month_orders_items: month_orders_items,
+                most_profitable_products: most_profitable_products,
+                most_popular_products: most_popular_products,
+                category_profit_pairs: category_revenue_pairs,
+            },
+        }
     end
-    
 
+    def show
+        trader_order_items = @trader.order_items
+
+        # Total profits of a category
+        category_total_revenue = trader_order_items.left_joins(:item).where("items.category_id = #{params[:category_id]}").sum('total') # The result
+        # Most popular products of a category
+        category_most_popular_products = trader_order_items.joins(:item).where("items.category_id = #{params[:category_id]}").select('name, quantity').group(:name).order('sum_quantity desc').limit(10).sum('quantity') # The result
+
+        render json: {
+            category_total_revenue: category_total_revenue,
+            category_most_popular_products: category_most_popular_products,
+        }
+    end
 end
